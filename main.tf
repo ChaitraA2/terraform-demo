0b6150b546af238d503f5ba106c9bb89c26209b5
Chaitra <chaitra.awslearn@gmail.com>
2025-09-10 15:06:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ resource "aws_instance" "my_instance" {
 	instance_type = var.instance_type
  }
 
-resource "aws_instance" "my_instance1" {
-	ami = var.instance_ami
-	count = var.instance_count
-	instance_type = var.instance_type
-}
+//resource "aws_instance" "my_instance1" {
+//	ami = var.instance_ami
+//	count = var.instance_count
+//	instance_type = var.instance_type
+//}
 
 Resource "aws_vpc" "my_vpc" {
 	cidr_block = "10.0.0.0/16"
